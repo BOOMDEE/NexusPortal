93ee21c4086b11418ec2264a463033045ed95f9b
BOOMDEE <102970970+BOOMDEE@users.noreply.github.com>
2026-02-03 19:21:44 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+/**
+ * NEXUS_CONFIG - REDIRECTION MATRIX
+ * SET YOUR LINKS BELOW
+ */
+const NEXUS_CONFIG = {
+    // DEFAULT: Where everyone goes by default
+    "default": "https://github.com/BOOMDEE",
+
+    // SPECIAL: For example, your birthday
+    "130703": "https://boomdee.github.io",
+
+    // EXAMPLES: Remove "//" at the start of the line to enable
+    // "111111": "https://a.com",
+    // "222222": "https://b.com"
+    "250002": "https://cd.boomdee.online"
+};
